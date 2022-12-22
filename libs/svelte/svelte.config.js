9227d74ff1ad38763f8b95b45d78b27cd0cb50c7
Snorre Strand <snorrekstrand@hotmail.com>
2022-12-22 17:53:26 +0800

    fix: remove windi

--- libs/svelte/svelte.config.js
+++ libs/svelte/svelte.config.js
@@ -1,8 +1,6 @@
-/* jshint esversion: 9 */
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import preprocess from 'svelte-preprocess';
-import { windi } from 'svelte-windicss-preprocess';
 
 const __dirname = dirname(fileURLToPath(import.meta.url)); // jshint ignore:line
 
@@ -12,26 +10,6 @@ const config = {
 		vite: () => ({
 			resolve: {
 				alias: {
-					'@shared/constants': resolve(
-						__dirname,
-						'../../../libs/shared/constants/src',
-					),
-					'@shared/frontend/utils': resolve(
-						__dirname,
-						'../../../libs/shared/frontend/utils/src',
-					),
-					'@shared/svelte': resolve(
-						__dirname,
-						'../../../libs/shared/svelte/src',
-					),
-					'@shared/types': resolve(
-						__dirname,
-						'../../../libs/shared/types/src',
-					),
-					'@shared/utils': resolve(
-						__dirname,
-						'../../../libs/shared/utils/src',
-					),
 					// eslint-disable-next-line sort-keys
 					$types: resolve(__dirname, './src/lib/types'),
 					$utils: resolve(__dirname, './src/lib/utils'),
@@ -42,7 +20,6 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about pre processors
 	preprocess: [
-		windi({}),
 		preprocess({
 			// WARNING: Mandatory for the postcss config (postcss.config.cjs) and Tailwind to be loaded
 			postcss: false,
